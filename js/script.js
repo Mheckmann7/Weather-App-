625d089db338b60171d4ff6a265f270b25aea9bd
Mira Heckmann <miraheckmann@gmail.com>
2020-10-14 14:35:15 -0700

    creates basic functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 //Constants and variables
 const  API_KEY = 'b428edff442fe38bb8486623bb43ce3b'; 
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+let weatherData;
 
 
 //Cached element references
-const $title = $('title');
-const $temp = $('temp'); 
-const $index = $('index'); 
-const $desc = $('desc'); 
+const $title = $('#title');
+const $temp = $('#temp'); 
+const $index = $('#index'); 
+const $desc = $('#desc'); 
 const $main = $('main');
 const $form = $('form');
 const $input = $('input[type="text"]');
@@ -21,9 +22,10 @@ function handleGetData(event) {
     userInput = $input.val();
     if(!userInput) return; 
 
-    $.ajax(BASE_URL + 'q=Dallas&units=metric&appid=' + API_KEY)
+    $.ajax(BASE_URL + 'q=' + userInput + '&units=metric&appid=' + API_KEY)
     .then(function(data) {
         console.log('Data: ', data); 
+        weatherData = data; 
         render(); 
     }, function(error) {
         console.log('Error: ', error); 
@@ -31,5 +33,8 @@ function handleGetData(event) {
 }
 
 function render() {
-    $title.text()
+    $title.text('Here is your weather report for ' + weatherData.name);
+    $temp.text('Temp ' + weatherData.main.temp + ' C'); 
+    $index.text('Feels like ' + weatherData.main.feels_like + ' C'); 
+    $desc.text('Looks like ' + weatherData.weather[0].description);
 }
